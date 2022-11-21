16e3723d512fa84c5a07873c55e883eefc1a6786
gouthamigouthami <gouthamikashaboina44@gmail.com>
2022-11-21 14:38:06 +0530

    source

---
+++ code/source/server_header.h
@@ -0,0 +1,9 @@
+#define sa struct sockaddr_in
+#define  maxlen 100
+#define u_int unsigned int
+#define client_listen 5
+#define data_size 10
+#define command_size 5
+#define  file_size 20
+#include "../include/Logger.h"
+#define server_path "./server_files/"
