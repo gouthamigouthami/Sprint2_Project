92c1e7850840fd757ebe009987bb489e23b04e21
Gangireddy-Deepika <116145103+Gangireddy-Deepika@users.noreply.github.com>
2022-11-22 18:37:05 +0530

    client_header.h
    client_header.h

---
+++ code/source/client_header.h
@@ -0,0 +1,19 @@
+#define MAX 500
+#define PORT 9000
+#define LOGIN_ATTEMPT 3
+#define MAXIMUM_NAME 25
+#define MAXIMUM_PSW 25
+#define MAXIMUM_PH_NO 25
+#define MAXIMUM_EMAIL 25
+#define MAXIMUM_ATTEMPT 3
+#define pHNUM_LIMIT 10
+#define MAX_LISTEN 15
+#define FILEPATH_SIZE 50
+#define DATA_SIZE 10
+#define CMD_SIZE 100
+#define FILENAME_SIZE 25
+#define BUFFER_SIZE20
+#define LISTEN_SIZE 20
+#define PATH "./client_files/"
+#include "../include/Logger.h"
+
