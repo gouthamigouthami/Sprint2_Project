21ca5af536178509b49162e2f8a93ff6c535988e
gouthamigouthami <118154126+gouthamigouthami@users.noreply.github.com>
2022-11-23 09:33:23 +0530

    source

--- code/source/server_header.h
+++
@@ -1,9 +0,0 @@
-#define sa struct sockaddr_in
-#define  MAXLEN 100
-#define u_int unsigned int
-#define CLIENT_LISTEN 5
-#define DATA_SIZE 10
-#define COMMAND_SIZE 5
-#define  FILE_SIZE 20
-#include "../include/Logger.h"
-#define SERVER_PATH"./server_files/"
