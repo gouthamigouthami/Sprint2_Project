964996abee555391d1afd0e11e13d3a84626afa7
Gangireddy-Deepika <116145103+Gangireddy-Deepika@users.noreply.github.com>
2022-11-25 19:02:56 +0530

    code

--- code/source/server_header.h
+++ code/source/server_header.h
@@ -9,8 +9,8 @@
 #define COMMAND_SIZE 5
 #define  FILE_SIZE 20
 #define PORT_1 9090
-#defile PROTOCOL 0
-#defile FILEHANDLE -1
+#define PROTOCOL 0
+#define FILEHANDLE -1
 #include "../include/LOGGER.H"
 #define SERVER_PATH "./server_files/"
 #endif
